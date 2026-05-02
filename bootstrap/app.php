319d03aecf72ec68e565f321bd9ecb08a8b3bb96
chhenlongsue-star <chhenlongsue@gmail.com.com>
2026-05-02 14:22:31 +0700

    fix ban

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->web(append: [
         \App\Http\Middleware\UpdateLastSeenAt::class,
+        \App\Http\Middleware\CheckBanned::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
