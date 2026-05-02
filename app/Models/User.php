eed918b9ee05fbc4693613a43ae83c7a30043bf4
chhenlongsue-star <chhenlongsue@gmail.com.com>
2026-05-02 12:14:53 +0700

    add relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -22,10 +23,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'google_id', // Added for Gmail Login
-        'avatar',    // Added for profile pictures
-        'role',      // Added for Admin/User roles
+        'google_id',    // Added for Gmail Login
+        'avatar',       // Added for profile pictures
+        'role',         // Added for Admin/User roles
         'last_seen_at', // Added for tracking user activity
+        'is_active',    // Added for Ban/Disable functionality
     ];
 
     /**
@@ -49,6 +51,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'last_seen_at' => 'datetime',
+            'is_active' => 'boolean', // Ensures 0/1 is treated as true/false
         ];
     }
 
@@ -61,9 +64,18 @@ class User extends Authenticatable
     }
 
     /**
-     * Relationship: A user can have many votes
+     * Relationship: A user can create many polls
+     * This fixes the "Call to undefined method polls()" error
      */
-    public function votes()
+    public function polls(): HasMany
+    {
+        return $this->hasMany(Poll::class);
+    }
+
+    /**
+     * Relationship: A user can cast many votes
+     */
+    public function votes(): HasMany
     {
         return $this->hasMany(Vote::class);
     }
